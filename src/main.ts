f025a82747f76428252aec8a68dca5ab6744a712
unknown <vlad6754687@gmail.com>
2024-03-05 23:39:06 +0700

    can return all fields from db

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,6 @@ async function bootstrap() {
   const PORT = process.env.PORT || 5000
   const app = await NestFactory.create(AppModule);
 
-  const prismaService = app.get(PrismaService)
-
   app.enableCors()
 
   await app.listen(PORT, () => console.log(`server start on ${PORT} port`));
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,5 +1,4 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { NotFoundError } from 'rxjs';
 import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
@@ -7,23 +6,17 @@ export class TaskService {
     constructor(private prisma: PrismaService) {}
     
     async getUsersPosts(dto: number) {
-        const posts_array = await this.prisma.iArrayTasks.findMany({
-          where: { userId: +dto },
-        });
+        const user = this.prisma.user.findUnique({
+            where: {id: +dto},
+            include: {
+                posts: {
+                    include: {
+                        todos: true
+                    }
+                }
+            }
+        })
 
-        let posts = []
-
-        for(let i = 0; i < posts_array.length; i++) {
-            posts.push(await this.prisma.iTask.findMany({
-                where: {iArrayTasksId: posts_array[i].id}
-            }))
-        }
-        
-        if(!posts_array) throw new NotFoundException('not found')
- 
-        return {
-            array: posts_array,
-            todos: posts
-        };
+        return user
       }
 }
