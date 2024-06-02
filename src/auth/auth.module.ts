e7d62f9166dae6a2bd7345fe318b32379ced46e3
vlad424 <vlad6754687@gmail.com>
2024-06-02 19:40:36 +0700

    nevermind

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { PrismaService } from 'src/prisma.service';
+import { PrismaService } from './prisma.service';
 import { JwtModule } from '@nestjs/jwt';
-import { getJwtConfig } from 'src/config/jwt.config';
+import { getJwtConfig } from './config/jwt.config';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtStrategy } from './jwt.strategy';
 
