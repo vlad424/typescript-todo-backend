ecfebc492bfbd1d7975c899ab9ba92bf408b8ae3
vlad424 <vlad6754687@gmail.com>
2024-06-02 18:58:50 +0700

    oh

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { PrismaService } from './prisma.service';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 5000
