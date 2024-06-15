e829b6997b86bbfca5a583c950e249a7f735738f
vlad424 <vlad6754687@gmail.com>
2024-06-15 17:05:32 +0700

    up to date

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { TaskController } from './task.controller';
-import { PrismaService } from './prisma.service';
-import { JwtStrategy } from './auth/jwt.strategy';
+import { PrismaService } from 'src/prisma.service';
+import { JwtStrategy } from 'src/auth/jwt.strategy';
 
 @Module({
   controllers: [TaskController],
