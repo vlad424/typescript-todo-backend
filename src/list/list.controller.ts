96006e872b64c6eba6cc9c3b430073849a0518a5
unknown <vlad6754687@gmail.com>
2024-06-24 01:45:45 +0700

    feat: added api for some routes

--- src/list/list.controller.ts
+++ src/list/list.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ListService } from './list.service';
 
-@Controller('list')
+@Controller('/workspace/:id/admin')
 export class ListController {
-  constructor(private readonly listService: ListService) {}
+  constructor(private readonly listService: ListService) {} 
+
+  @Get()
+  async getAdminListsAndTasks(@Param() data: {id: number}) {
+    return this.listService.getAdminListsAndTasks(data.id)
+  }
+  @Post()
+  async putListOrComment(@Param() data: {id: number}, @Body() req: {action: 'PUT LIST' | 'PUT COMMENT', data: string}) {
+    if(req.action === 'PUT LIST') return this.listService.putListOrComment(data.id, req.data)
+  }
 }
--- src/list/list.service.ts
+++ src/list/list.service.ts
@@ -1,7 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class ListService {
   constructor(private prisma: PrismaService) {}
+
+  async getAdminListsAndTasks(id: number) {
+    const adminLists = await this.prisma.iLists.findMany({
+      where: {
+        userId: +id
+      },
+      include: {
+        lists: true
+      }
+    })
+    const users = await this.prisma.user.findMany({
+      where: {
+        role: "user"
+      },
+      include: {
+        posts: {
+          include: {
+            todos: true
+          }
+        }
+      }
+    })
+    //admin comments
+
+    return {adminLists, users}
+  }
+  async putListOrComment(id: number, data: string) {
+    const createList = await this.prisma.iLists.create({
+      data: {
+        name: data,
+        userId: +id,
+        addressee: []
+      }
+    })
+    return {
+      status: HttpStatus.CREATED,
+      msg: 'list created',
+      data: {
+        createList
+      }
+    }
+  }
 }
