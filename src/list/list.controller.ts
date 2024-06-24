8145d7b0d7c7b244a05731fed404db8c80b47758
unknown <vlad6754687@gmail.com>
2024-06-24 16:09:24 +0700

    reworked api instructions

--- src/list/list.controller.ts
+++ src/list/list.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { ListService } from './list.service';
 
 @Controller('/workspace/:id/admin')
@@ -10,7 +10,12 @@ export class ListController {
     return this.listService.getAdminListsAndTasks(data.id)
   }
   @Post()
-  async putListOrComment(@Param() data: {id: number}, @Body() req: {action: 'PUT LIST' | 'PUT COMMENT', data: string}) {
-    if(req.action === 'PUT LIST') return this.listService.putListOrComment(data.id, req.data)
+  async putListOrComment(@Param() data: {id: number}, @Body() req: {action: 'PUT LIST' | 'PUT COMMENT' | 'PUT LIST EL', data: string | {name: string, listId: number}}) {
+    if(req.action === 'PUT LIST') return this.listService.putList(data.id, req.data as string)
+    if(req.action === 'PUT LIST EL') return this.listService.putListEl(req.data)
+  }
+  @Delete()
+  async deleteListOrComment(@Param() data: {id: number}, @Body() req: {action: 'DELETE LIST' | 'DELETE COMMENT', data: number}) {
+    if(req.action === 'DELETE LIST') return this.listService.deleteListOrComment(req.data)
   }
 }
--- src/list/list.service.ts
+++ src/list/list.service.ts
@@ -8,42 +8,80 @@ export class ListService {
   async getAdminListsAndTasks(id: number) {
     const adminLists = await this.prisma.iLists.findMany({
       where: {
-        userId: +id
+        userId: +id,
       },
       include: {
-        lists: true
-      }
-    })
+        lists: true,
+      },
+    });
     const users = await this.prisma.user.findMany({
       where: {
-        role: "user"
+        role: 'user',
       },
       include: {
         posts: {
           include: {
-            todos: true
-          }
-        }
-      }
-    })
+            todos: true,
+          },
+        },
+      },
+    });
     //admin comments
 
-    return {adminLists, users}
+    return { adminLists, users };
   }
-  async putListOrComment(id: number, data: string) {
+  async putList(id: number, data: string) {
     const createList = await this.prisma.iLists.create({
       data: {
         name: data,
         userId: +id,
-        addressee: []
-      }
-    })
+        addressee: [],
+      },
+    });
     return {
       status: HttpStatus.CREATED,
       msg: 'list created',
       data: {
-        createList
-      }
+        createList,
+      },
+    };
+  }
+  async putListEl(req: any) {
+    const list = await this.prisma.iList.create({
+      data: {
+        name: req.name,
+        dateCreate:
+          new Date().toLocaleDateString().toString() +
+          ' ' +
+          new Date().toLocaleTimeString().toString(),
+        dateAt:
+          new Date().toLocaleDateString().toString() +
+          ' ' +
+          new Date().toLocaleTimeString().toString(),
+        desc: 'Описание',
+        text_color: '#000',
+        IListsId: +req.listId
+      },
+    });
+
+    return {
+      status: HttpStatus.OK,
+      msg: `list el with id: ${list.id} created`,
+      list
     }
+  }
+  async deleteListOrComment(data: number) {
+    const deletedList = await this.prisma.iLists.delete({
+      where: { id: +data },
+      include: {
+        lists: true,
+      },
+    });
+
+    return {
+      status: HttpStatus.OK,
+      msg: `list with id: ${data} deleted`,
+      deletedList,
+    };
   }
 }
