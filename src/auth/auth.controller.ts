abc58051ce6dac38ecb13e93be57d8ff40b7b51a
vlad424 <vlad@6754687@gmail.com>
2024-06-30 00:01:06 +0700

    again

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Get, HttpCode, Post, Redirect, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Post,
+  Redirect,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDtoAuth, AuthDtoRegister } from './dto/auth.dto';
 import { refreshTokenDto } from './dto/refreshToken.dto';
@@ -8,33 +18,29 @@ import { AuthGuard } from '@nestjs/passport';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @HttpCode(200)
-  @Get('/')
-  async serverUp() {
-    return {
-      msg: "server up"
-    }
+  @HttpCode(200) @Get('/') async serverUp() {
+    return { msg: 'server up' };
   }
 
   @UsePipes(new ValidationPipe())
   @HttpCode(200)
   @Post('/register')
   async register(@Body() dto: AuthDtoRegister) {
-    return this.authService.register(dto)
+    return this.authService.register(dto);
   }
 
   @UsePipes(new ValidationPipe())
   @HttpCode(200)
   @UseGuards(AuthGuard('jwt'))
-  @Post('/login/access-token') 
+  @Post('/login/access-token')
   async getNewToken(@Body() dto: refreshTokenDto) {
-    return this.authService.getNewToken(dto.refreshToken)
+    return this.authService.getNewToken(dto.refreshToken);
   }
 
   @UsePipes(new ValidationPipe())
   @HttpCode(200)
   @Post('/')
   async login(@Body() dto: AuthDtoAuth) {
-    return this.authService.login(dto)
+    return this.authService.login(dto);
   }
 }
