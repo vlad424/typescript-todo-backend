efb3003d08d8daff7890a5093f4ce07361813766
vlad424 <vlad6754687@gmail.com>
2024-06-29 15:25:58 +0700

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,21 +5,7 @@ async function bootstrap() {
   const PORT = process.env.PORT || 5000
   const app = await NestFactory.create(AppModule);
 
-  var whitelist = ['http://95.163.223.141:3000', 'http://95.163.223.141:3000'];
-app.enableCors({
-origin: function (origin, callback) {
-  if (whitelist.indexOf(origin) !== -1) {
-    console.log("allowed cors for:", origin)
-    callback(null, true)
-  } else {
-    console.log("blocked cors for:", origin)
-    callback(new Error('Not allowed by CORS'))
-  }
-},
-allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
-methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS",
-credentials: true,
-});
+  app.enableCors()
 
   await app.listen(PORT, () => console.log(`server start on ${PORT} port`));
 }
