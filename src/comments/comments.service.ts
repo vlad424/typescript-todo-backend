ec6598c8975815ba59ddfe78db59fc1b87c4f0a6
vlad424 <vlad6754687@gmail.com>
2024-06-30 19:12:07 +0700

    feat: now comment is save

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -5,54 +5,65 @@ import { PrismaService } from 'src/prisma.service';
 export class CommentsService {
   constructor(private prisma: PrismaService) {}
 
-  async GetUserLists(userId : number) {
+  async GetUserLists(userId: number) {
     const lists = await this.prisma.iLists.findMany({
       where: {
         addressee: {
-          has: +userId
-        }
+          has: +userId,
+        },
       },
       include: {
-        lists: true
-      }
-    })
+        lists: true,
+      },
+    });
 
-    return lists
+    return lists;
   }
-  async PostComment(data: {msg: string, addr: number}) {
+  async PostComment(data: { msg: string; addr: number }) {
     const task = await this.prisma.iTask.findUnique({
-      where: {id: +data.addr}
-    })
+      where: { id: +data.addr },
+    });
 
-    const findComment = await this.prisma.comment.findMany({
-      where: { todoId: +task.id}
-    })
+    const findComment = await this.prisma.comment.findFirst({
+      where: { todoId: +task.id },
+    });
 
-    if(!findComment) {
+    if (!findComment) {
       const comment = await this.prisma.comment.create({
         data: {
           message: data.msg,
-          todoId: +data.addr
-        }
-      })
+          todoId: +data.addr,
+        },
+      });
       const changedTask = await this.prisma.iTask.update({
-        where: {id: +data.addr},
+        where: { id: +data.addr },
         data: {
-          commentId: comment.id
-        }
-      })
+          commentId: comment.id,
+        },
+      });
 
-      return changedTask
-    }
-    else {
-      const comment = await this.prisma.comment.updateMany({
+      return changedTask;
+    } else {
+      const commentSource = await this.prisma.comment.findMany({
+        where: { todoId: +data.addr },
+        select: { id: true },
+      });
+      const comment = await this.prisma.comment.update({
+        where: {
+          id: +commentSource[0].id,
+        },
         data: {
           message: data.msg,
-          todoId: +data.addr
-        }
-      })
+        },
+      });
+      const task = await this.prisma.iTask.update({
+        where: { id: +data.addr },
+        data: {
+          commentId: comment.id,
+        },
+      });
 
-      return comment
+      return comment;
     }
   }
 }
